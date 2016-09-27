da41a0437aff2235371c85c7ca1e30c959a60a7d
matthiashofmann <mhofm@me.com>
2016-09-27 15:40:13 +0900

    add firebase, project setup

--- itjapan-social/LoginViewController.swift
+++ itjapan-social/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  itjapan-social
 //
 //  Created by Matthias Hofmann on 27.09.16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()

1f460b5662c63733afb868f56a41264e658ee862
matthiashofmann <mhofm@me.com>
2016-09-28 16:25:52 +0900

    add feed view ui

---
+++ itjapan-social/CustomView.swift
@@ -0,0 +1,24 @@
+//
+//  CustomView.swift
+//  itjapan-social
+//
+//  Created by Matthias Hofmann on 28.09.16.
+//  Copyright © 2016 MatthiasHofmann. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // add dropshadow
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2.0
+    }
+
+
+}

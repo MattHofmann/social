bc016c80c8954b6b735e5d4540c6ceb087230c00
matthiashofmann <mhofm@me.com>
2016-09-28 13:31:42 +0900

    add Facebook authentification

--- itjapan-social/AppDelegate.swift
+++ itjapan-social/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        // Firebase
         FIRApp.configure()
+        // Facebook
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
 
@@ -44,6 +49,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    // Facebook: Track App Installs and App Opens
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        //return FBSDKApplicationDelegate.sharedInstance().application(appl, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?, annotation: nil)
+        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String?, annotation: nil)
+    }
 
 }
 
--- itjapan-social/LoginViewController.swift
+++ itjapan-social/LoginViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -20,6 +23,34 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // facebook auth
+    @IBAction func facebookButtonPressed(_ sender: AnyObject) {
+        // create loginmanager
+        let facebookLogin = FBSDKLoginManager()
+        // request email
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("DEV: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("DEV: User cancelled Facebook authentication")
+            } else {
+                print("DEV: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    // firebase auth method
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("DEV: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("DEV: Successfully authenticated with Firebase")
+            }
+        })
+    }
 
 }
 

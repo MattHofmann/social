ae65eac05b9b3ad1c6ecebf608acd127510e8c5a
matthiashofmann <mhofm@me.com>
2016-09-28 16:56:17 +0900

    add tableview in FeedViewController

--- itjapan-social/FeedViewController.swift
+++ itjapan-social/FeedViewController.swift
@@ -10,20 +10,32 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        //return UITableViewCell()
+    }
+    
+    
     @IBAction func signOutButtonPressed(_ sender: UIButton) {
         let keychainResult = KeychainWrapper.defaultKeychainWrapper().removeObjectForKey(KEY_UID)
         print("DEV: Id removed from keychain - \(keychainResult)")
---
+++ itjapan-social/PostCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostCell.swift
+//  itjapan-social
+//
+//  Created by Matthias Hofmann on 28.09.16.
+//  Copyright © 2016 MatthiasHofmann. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}

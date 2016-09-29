b5e67b3821ad38bb734d0ca07dbbae7c0903d450
matthiashofmann <mhofm@me.com>
2016-09-29 11:56:51 +0900

    add Firebase get request

--- itjapan-social/FeedViewController.swift
+++ itjapan-social/FeedViewController.swift
@@ -14,6 +14,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,7 +25,21 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         // listener
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
+            // print(snapshot.value)
+            self.posts = []
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postId: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
     
     }
@@ -33,10 +49,14 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("DEV: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         //return UITableViewCell()
     }
---
+++ itjapan-social/Post.swift
@@ -0,0 +1,57 @@
+//
+//  Post.swift
+//  itjapan-social
+//
+//  Created by Matthias Hofmann on 29.09.16.
+//  Copyright © 2016 MatthiasHofmann. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    private var _caption: String!
+    private var _imageUrl: String!
+    private var _likes: Int!
+    private var _postId: String!
+    
+    var caption: String {
+        return _caption
+    }
+    
+    var imageUrl: String {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var postId: String {
+        return _postId
+    }
+    
+    init(caption: String, imageUrl: String, likes: Int) {
+        self._caption = caption
+        self._imageUrl = imageUrl
+        self._likes = likes
+    }
+    
+    init(postId: String, postData: Dictionary<String, AnyObject>) {
+        self._postId = postId
+        
+        if let caption = postData["caption"] as? String {
+            self._caption = caption
+        }
+        
+        if let imageUrl = postData["imageUrl"] as? String {
+            self._imageUrl = imageUrl
+        }
+        
+        if let likes = postData["likes"] as? Int {
+            self._likes = likes
+        }
+        
+    }
+    
+}
